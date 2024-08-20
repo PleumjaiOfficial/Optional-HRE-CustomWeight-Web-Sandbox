ea32ec6efe5e0f5b9a255de9e04ababf425e6676
Thornthan yasukam <82392844+PleumjaiOfficial@users.noreply.github.com>
2024-08-20 14:16:49 +0700

    Fixed title

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ columns = ['PERSON_ID', 'SOURCE'] + scores_columns
 data = pd.DataFrame(columns=columns)
 
 # Streamlit app title
-st.title("Employee Score Input Simulation")
+st.title("For You HR BPNEXT✨, Sandbox for Toptalent Simulation from Weighted Score")
 # st.subheader("How many employees do you want to compare?")
 # num_emp = st.number_input("Number of employees", min_value=1, max_value=100, value=3)
 
